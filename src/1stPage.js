c337b7a62e9ef9ad36e0d8a11fae43fcd01e9719
gelaylay <03angela.07ayadam@gmail.com>
2023-11-26 17:34:49 +0800

    new file:   public/Images/Angela.jpg 	new file:   public/Images/Anne.png 	new file:   public/Images/Julliana.png 	new file:   public/Images/Mark.png 	new file:   public/Images/bg.png 	new file:   public/Images/bg_1.png 	new file:   public/Images/logo.png 	new file:   public/Images/teachmanage.png 	new file:   public/Images/title.png 	new file:   src/1stPage.js 	new file:   src/Fonts/Bauhus.otf 	new file:   src/Fonts/CourierPrime-Regular.ttf 	new file:   src/Fonts/CreteRound-Regular.ttf 	new file:   src/Fonts/Lateef-Regular.ttf 	new file:   src/Fonts/Literata.ttf 	new file:   src/Fonts/PlayfairDisplay.ttf 	new file:   src/Fonts/Poppins.ttf 	new file:   src/Fonts/Raleway.ttf 	new file:   src/Fonts/Righteous.ttf 	new file:   src/Fonts/VesperLibre-Regular.ttf

---
+++ src/1stPage.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { Box, Fab,Grid} from '@mui/material';
+import { Link, Link as RouterLink } from 'react-router-dom';
+
+import Aboutus from './aboutus';
+
+import './style.css';
+
+function App() {
+  const [showAboutUs, setShowAboutUs] = useState(false);
+
+  const handleAboutUsClick = () => {
+    setShowAboutUs(true);
+  };
+  const handleCloseAboutUs = () => {
+    setShowAboutUs(false);
+  };
+
+  return (
+    <div
+        style={{
+          backgroundColor: '#359DD9',
+          height: '100vh',
+          position: 'relative',
+        }}
+      >
+        <Box
+          sx={{
+            backgroundImage: `url(Images/bg.png)`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100%',
+          }}
+        >
+      <Aboutus open={showAboutUs} handleClose={handleCloseAboutUs} />
+      <Grid container spacing={0}>
+        <Grid item xs={12}>
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'right',
+          marginBottom:'20vh'
+        }}>
+          <Fab variant="extended" color="#d9d9d9" sx={{ fontSize: '1rem', marginRight: '10px', marginTop: '10px', fontFamily: 'Righteous' }} onClick={handleAboutUsClick}>
+            About Us
+          </Fab>
+
+          <Link component={RouterLink} to="/signin_signup" underline="none">
+          <Fab variant="extended" color="#d9d9d9" sx={{ fontSize: '1rem', marginTop: '10px', marginRight: '10px', fontFamily: 'Righteous' }}>
+            Get Started
+          </Fab>
+          </Link>
+        </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center',ml:'30px' }}>
+            <img
+              src="Images/title.png"
+              style={{ width: '50%' }}
+              alt="Title"
+            />
+          </Box>
+
+        </Grid>
+
+      </Grid>
+          </Box>
+        
+
+    </div>
+  );
+}
+
+export default App;
